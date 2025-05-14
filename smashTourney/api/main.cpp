12e7bc2d969cd740201160f2c513ee3373e7ed41
nicholas hutter <nicholas.hutter@live.com>
2025-05-14 16:17:23 -0500

    current snapshot logger untested

--- smashTourney/api/main.cpp
+++ smashTourney/api/main.cpp
@@ -1,12 +1,14 @@
-#include "libs/pch.h"
-#include "boost/uuid/uuid.hpp"
-#include "boost/uuid/uuid_generators.hpp"
+#include "libs/pch.h";
+#include "api/services/Logger.cpp";
+#include "boost/uuid/uuid.hpp";
+#include "boost/uuid/uuid_generators.hpp";
 
 int main()
 {
     std::cout << "Hello World!";
     std::cin.get();
 
-    
+    Logger logger = Logger::getInstance();
+
     return 0;
 }
